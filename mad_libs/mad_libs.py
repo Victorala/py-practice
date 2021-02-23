f31ad3e1f4a88325ae072027da691bdfd175095c
Victorala <42777037+Victorala@users.noreply.github.com>
2021-02-23 15:44:23 -0500

    Add files via upload

---
+++ mad_libs/mad_libs.py
@@ -0,0 +1,11 @@
+import random
+
+f = open('madlibs.txt','r')
+madlibText = f.readlines()
+
+madlib = random.choice(madlibText)
+
+noun = input('Enter a noun: ')
+madlib = madlib.replace('blank', noun)
+
+print(madlib)
